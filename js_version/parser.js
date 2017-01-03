50f2b6609343a04c20b81a5abc3015c108d3f82f
Vladimir Rubin <vladimir.rubin@qweeco.com>
2017-01-03 18:54:33 +0300

    Implement JS parser version

---
+++ js_version/parser.js
@@ -0,0 +1,78 @@
+var https = require('https');
+
+var url = 'https://raw.githubusercontent.com/1ppm/1ppmLog/master/HallOfFame.md';
+
+var baseRegex = /\#{1,2}\s\[([^\]]+)\]\(([^)]+)\)(\n{1,}(\|.+\|))+/g;
+var USERNAME_GROUP = 1;
+var USER_URL_GROUP = 2;
+var PROJECTS_GROUP = 0;
+
+var projectsRegex = /^\|([0-9\s\/]+)\|(.+?)\|(.+?)\|((.+)\|)?/gm;
+var PROJECT_DATE_GROUP = 1;
+var PROJECT_NAME_GROUP = 2;
+var PROJECT_DESCRIPTION_GROUP = 3;
+var PROJECT_STATUS_GROUP = 5;
+
+var request = https.get(url, function(res) {
+    var body = [];
+    res.on('data', function(chunk) {
+        body.push(chunk);
+    }).on('end', function() {
+        content = Buffer.concat(body).toString();
+        var result = convertMDtoJSON(content);
+        console.log(JSON.stringify(result, null, 4));
+    });
+});
+
+function convertMDtoJSON(mdContent){
+  var data = [];
+  var usersMatch;
+  while ((usersMatch = baseRegex.exec(mdContent)) !== null) {
+    if (usersMatch.index === baseRegex.lastIndex) {
+        baseRegex.lastIndex++;
+    }
+    
+    var userData = {
+        username: usersMatch[USERNAME_GROUP],
+        githubUrl: '',
+        otherUrl: '',
+        projects: [] 
+    };
+
+    userUrl = usersMatch[USER_URL_GROUP];
+    if (userUrl.indexOf('github') > -1) {
+        userData['githubUrl'] = userUrl;
+    } else {
+        userData['otherUrl'] = userUrl;
+    }
+    
+    var projectsMD = usersMatch[PROJECTS_GROUP];
+    var projectsMatch;
+    while ((projectsMatch = projectsRegex.exec(projectsMD)) !== null) {
+        if (projectsMatch.index === projectsRegex.lastIndex) {
+            projectsRegex.lastIndex++;
+        }
+        // Date processing
+        var projectDate = projectsMatch[PROJECT_DATE_GROUP].trim().split('/').reverse().join('');
+        // Name processing
+        var projectName = projectsMatch[PROJECT_NAME_GROUP].trim();
+        var nameParts = projectName.split(']');
+        if (nameParts.length) {
+            projectName = nameParts[0].replace('[','');
+        }
+        // Description processing
+        var projectDescription = projectsMatch[PROJECT_DESCRIPTION_GROUP].trim();
+        var projectStatus = projectsMatch[PROJECT_STATUS_GROUP] ? projectsMatch[PROJECT_STATUS_GROUP].trim() : '';
+        project = {
+            [projectDate]: {
+                name: projectName,
+                description: projectDescription,
+                status: projectStatus
+            }
+        }
+        userData.projects.push(project);
+    }
+    data.push(userData);
+  }
+  return data;
+}
